a1f99e2f020cd3b746703c0701f0db2b2dbfae41
MHaubenstock <Michael@usf-gold-wifi-226-3-155.laptops.usf.edu>
2014-11-04 10:58:56 -0500

    Started movement builder with IK

---
+++ NOW Bot Modeler/Assets/Scripts/MovementBuilder.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementBuilder : MonoBehaviour
+{
+	//For mouse orbiting
+	public Transform target;
+	public float distance = 10.0f;
+	public float xSpeed = 250.0f;
+	public float ySpeed = 120.0f;
+	public float yMinLimit = -20f;
+	public float yMaxLimit = 80f;
+	private float x = 0.0f;
+	private float y = 0.0f;
+	//
+
+	// Use this for initialization
+	void Start ()
+	{
+		var angles = transform.eulerAngles;
+	    x = angles.y;
+	    y = angles.x;
+
+		// Make the rigid body not change rotation
+	   	if (rigidbody)
+			rigidbody.freezeRotation = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void LateUpdate()
+	{
+		if (target && Input.GetMouseButton(0)) 
+		{
+	        x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+	        y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+	 		
+	 		y = ClampAngle(y, yMinLimit, yMaxLimit);
+	 		       
+	        Quaternion rotation = Quaternion.Euler(y, x, 0);
+	        Vector3 position = rotation * new Vector3(0.0f, 0.0f, -distance) + target.position;
+	        
+	        transform.rotation = rotation;
+	        transform.position = position;
+	    }
+	}
+
+	static float ClampAngle (float angle, float min, float max)
+	{
+		if (angle < -360)
+			angle += 360;
+
+		if (angle > 360)
+			angle -= 360;
+
+		return Mathf.Clamp (angle, min, max);
+	}
+}
---
+++ NOW Bot Modeler/Assets/Scripts/ToppleHandler.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToppleHandler : MonoBehaviour
+{
+	public float correctionPower = 10;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rigidbody.centerOfMass = transform.position - Vector3.up * 10;
+		//rigidbody.centerOfMass = new Vector3(transform.position.x, - 20, transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Debug.DrawRay(transform.position, transform.up * 10, Color.blue);
+		
+		
+		//Get angle off of completely vertical
+		float angleBetween = Vector3.Angle(transform.TransformPoint(transform.up), Vector3.up);
+		/*
+		Debug.Log(angleBetween);
+		
+		//If angle is close to 180 then the bot fell over and you lose a point
+		*/
+
+
+		//Apply rotational force to self-correct the NAO bot's orientation
+		float xCorrection = Mathf.Min(360 - transform.rotation.x, transform.rotation.x);
+		float zCorrection = Mathf.Min(360 - transform.rotation.z, transform.rotation.z);
+
+		//transform.rigidbody.AddTorque(new Vector3(xCorrection, 0, zCorrection) * (angleBetween / 180) * correctionPower, ForceMode.Acceleration);
+		Debug.DrawRay(transform.position + transform.up * 2, (new Vector3(xCorrection, 0, zCorrection)).normalized * 4, Color.red);
+	}
+}
